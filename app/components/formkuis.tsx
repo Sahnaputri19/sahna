774a9198d9a36018f6f38cf5f99d64e7cd1267d4
Sahnaputri19 <sahnaputriy@gmail.com>
2024-10-21 11:00:46 +0700

    commit

---
+++ app/components/formkuis.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useState } from "react";
+
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+    <section className="container mx-auto p-4 text-center">
+     <h2>Kuis Kota</h2>
+      <p>
+        Ibu Kota Jawa Barat adalah?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+          className="text-black p-3 rounded-lg text-center"
+        />
+        <br />
+        <button disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
+      </section>
+    </>
+  );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'bandung'
+      if (shouldError) {
+        reject(new Error('Jawaban bagus tapi salah. Coba lagi!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
